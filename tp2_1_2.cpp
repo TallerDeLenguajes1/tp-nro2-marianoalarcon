231b1263119f220051b5df0e41f86aac8fa729ec
Mariano Alarcon <mariano.alarcon@hotmail.com>
2019-03-30 00:09:17 -0300

    Punto 2 modificado en rama Opcion2

--- tp2_1_2.cpp
+++ tp2_1_2.cpp
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 
 #define N 4
 #define M 5
 int main(void)
-{
+{   
+	srand(time(NULL));
 	int f,c;
 	double mt[N][M];
-	printf("SE MODIFICO EN LA RAMA OPCION2 \n");
+	printf("SE MODIFICO EN LA RAMA OPCION\n");
 	for(f = 0;f<N; f++)
 	{
 	 	for(c = 0;c<M; c++)
 		{
-			mt[f][c]=0;
+			mt[f][c]=rand() % 15+0; 
 		}
 	}
 
@@ -20,7 +23,7 @@ int main(void)
 	{
 	 	for(c = 0;c<M; c++)
 		{
-			printf("%lf", mt[f][c]);
+			printf("%.1f | ", *(*(mt+f)+c));
 		}
 	printf("\n");
 	}
